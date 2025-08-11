598a62ca7ff784e55bba49a7be472e60c7af1ce0
fz <fz@konfuzio.com>
2025-08-11 21:28:28 +0200

    Remove proprietary module references from v1.2.0

--- app/proprietary/src/main/java/stirling/software/proprietary/security/model/exception/BackupNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package stirling.software.proprietary.security.model.exception;
-
-public class BackupNotFoundException extends RuntimeException {
-    public BackupNotFoundException(String message) {
-        super(message);
-    }
-}
